cbab0bbda25962b009b99ae40c5f5c4c047aaeb1
DUC DO <duc1023as@gmail.com>
2022-08-26 16:55:39 +0700

    Exception Example

--- Exception/exception.cpp
+++ Exception/exception.cpp
@@ -1,6 +1,20 @@
 #include<iostream>
 
+double division(const int &number1,const int &number2){
+    if(!number2){
+        throw "Attempted to divide by zero";
+    }
+    return number1/number2;
+}
+
 int main(){
-    std::cout<<"Hello";//bbbb
-    return 0;//aaaa
+    std::cout<<"Exception Example"<<std::endl;
+    int number1,number2;
+    std::cin>>number1>>number2;
+    try{
+        std::cout<<division(number1,number2);
+    }catch(const char *e){
+        std::cout<<e<<std::endl;
+    }
+    return 0;
 }
