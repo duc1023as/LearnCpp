51188c0805a5c87d85af43405f262ab88ff6ea4f
DUC DO <duc1023as@gmail.com>
2022-08-27 17:45:05 +0700

    Add Destructor

--- Class/class.cpp
+++ Class/class.cpp
@@ -5,6 +5,9 @@ class Student{
     public:
         int age;
         std::string name;
+        Student(){
+            std::cout<<"Constructor"<<std::endl;
+        }
         Student(std::string name,int age){
             this->age = age;
             this->name = name;
@@ -12,6 +15,9 @@ class Student{
         Student(Student &anotherStudent){
             this->name = anotherStudent.name;
             this->age = anotherStudent.age;
+        }
+        ~Student(){
+            std::cout<<"Destructor"<<std::endl;
         }
         void display(){
             std::cout<<name<<std::endl<<age<<std::endl;
